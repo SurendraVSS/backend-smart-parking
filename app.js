b019e21aeaec1f1517b41d5004fbb1ce60fb39bf
SurendraVSS <106951672+SurendraVSS@users.noreply.github.com>
2023-04-03 07:17:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const auth = require("./middleware/auth");
 var bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 var cors = require('cors');
+const Razorpay = require("razorpay");
+
 
 const emergency_number = require("./model/emergency_number");
 const notice = require("./model/notice");
@@ -29,7 +31,58 @@ app.use(
     extended: true,
   })
 );
+var razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
+
+app.post("/verification", (req, res) => {
+  const secret = "razorpaysecret";
+
+  console.log(req.body);
+
+  const shasum = crypto.createHmac("sha256", secret);
+  shasum.update(JSON.stringify(req.body));
+  const digest = shasum.digest("hex");
+
+  console.log(digest, req.headers["x-razorpay-signature"]);
+
+  if (digest === req.headers["x-razorpay-signature"]) {
+    console.log("request is legit");
+    res.status(200).json({
+      message: "OK",
+    });
+  } else {
+    res.status(403).json({ message: "Invalid" });
+  }
+});
+
+
+app.post("/razorpay", async (req, res) => {
+  const payment_capture = 1;
+  const amount = 500;
+  const currency = "INR";
+
+  const options = {
+    amount,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture,
+  };
+
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    res.status(200).json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 //meeting goes here
 
 app.get("/api/emergency_number", async (req, res) => {
